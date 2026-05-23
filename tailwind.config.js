b82eb5bafdc883cc64071a61842df7c0202de583
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-23 12:27:25 +0630

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
