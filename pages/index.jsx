d8341d43deebf3cbc5dc7af62bb32e3381886200
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-05-23 12:34:09 +0630

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,12 @@
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import AppCollectionSection from "../components/AppCollectionSection";
 import AboutSection from "../components/AboutSection";
 import SkillsCloud from "../components/SkillsCloud";
+import AppCollectionSection from "../components/AppCollectionSection";
+import EducationSection from "../components/EducationSection";
+import BlogSection from "../components/BlogSection";
+import ContactSection from "../components/ContactSection";
 import DarkModeToggle from "../components/DarkModeToggle";
 import ParticlesBackground from "../components/ParticlesBackground";
 import ResumeDownloadButton from "../components/ResumeDownloadButton";
@@ -14,7 +17,10 @@ export default function Home() {
     <>
       <Head>
         <title>Moe Kyaw Aung · Senior Android Developer</title>
-        <meta name="description" content="Senior Android Developer portfolio by Moe Kyaw Aung (Dev-moe-kyawaung)" />
+        <meta
+          name="description"
+          content="Senior Android Developer portfolio by Moe Kyaw Aung (Dev-moe-kyawaung)"
+        />
       </Head>
 
       <ParticlesBackground />
@@ -24,9 +30,12 @@ export default function Home() {
 
       <AboutSection />
       <SkillsCloud />
+      <EducationSection />
       <AppCollectionSection />
+      <BlogSection />
+      <ContactSection />
 
-      {/* page အလုံးစုံ အဆုံးမှာ CV download button ထည့် */}
+      {/* page အဆုံးမှာ CV ဒေါင်းခလုတ် */}
       <div className="px-8 py-10">
         <div className="mx-auto max-w-2xl text-center">
           <ResumeDownloadButton />
